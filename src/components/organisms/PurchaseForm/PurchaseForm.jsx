ea622b1704f6853778cc18b2a42c9dbb36a01b7d
Drew <glavich_drew@yahoo.com>
2020-04-22 23:27:31 +0900

    Add skeleton for firebase call

--- src/components/organisms/PurchaseForm/PurchaseForm.jsx
+++ src/components/organisms/PurchaseForm/PurchaseForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { uploadPurchase } from "../../../firebase/utils";
 import {
   FormControl,
   InputLabel,
---
+++ src/firebase/utils.js
@@ -0,0 +1,5 @@
+import { db } from "./config";
+
+export function uploadPurchase(purchase) {
+  console.log("uploading purchase...");
+}
