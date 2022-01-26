ce703705afcd5e6f9d2aded528ebc4709086f7a0
Drew <glavich_drew@yahoo.com>
2022-01-26 23:12:12 +0900

    using environment variable for API_URL

--- src/api/mutations/addTransaction.ts
+++ src/api/mutations/addTransaction.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { Transaction, TransactionDto } from "../../types";
-import { baseUrl } from "../baseUrl";
 
 export async function addTransaction(
   transactionDto: TransactionDto
 ): Promise<Transaction> {
   const response = await axios
-    .post(`${baseUrl}/transactions`, transactionDto)
+    .post(`${process.env.API_URL}/transactions`, transactionDto)
     .catch(() => {
       throw new Error("Failed to post!");
     });
