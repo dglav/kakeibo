4345fe5cb0b1bb6e7966c84409090fe65cbb69c7
Drew <glavich_drew@yahoo.com>
2022-02-11 16:32:03 +0900

    feat: added edit button to each transaction card

--- src/components/TransactionCard.tsx
+++ src/components/TransactionCard.tsx
@@ -1,6 +1,7 @@
 import { format } from "date-fns";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, HStack, IconButton, Text } from "@chakra-ui/react";
 import { Transaction } from "services/transactions.service";
+import { EditIcon } from "@chakra-ui/icons";
 
 type Props = {
   transaction: Transaction;
@@ -9,10 +10,17 @@ type Props = {
 const TransactionCard = ({ transaction }: Props): React.ReactElement => {
   return (
     <Box shadow="md" width="100%" padding={4}>
-      <Heading pb={2}>{transaction.name}</Heading>
-      <Text>Amount: {transaction.amount}</Text>
-      <Text>Envelope: {transaction.envelope?.name ?? "Unknown Envelope"}</Text>
-      <Text>Date: {format(new Date(transaction.date), "yyyy/MM/dd")}</Text>
+      <HStack justifyContent="space-between">
+        <Box>
+          <Heading pb={2}>{transaction.name}</Heading>
+          <Text>Amount: {transaction.amount}</Text>
+          <Text>
+            Envelope: {transaction.envelope?.name ?? "Unknown Envelope"}
+          </Text>
+          <Text>Date: {format(new Date(transaction.date), "yyyy/MM/dd")}</Text>
+        </Box>
+        <IconButton aria-label="Search database" icon={<EditIcon />} />
+      </HStack>
     </Box>
   );
 };
