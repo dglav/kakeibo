0bdae9d6842f10f4440bc9782c43e5fb1d0431b0
Drew <glavich_drew@yahoo.com>
2022-02-20 01:16:56 +0900

    feat: delete envelope (as long as it is not used in a transaction)

--- src/components/EnvelopeList/EnvelopeCard.tsx
+++ src/components/EnvelopeList/EnvelopeCard.tsx
@@ -1,15 +1,71 @@
-import { Text } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Text,
+} from "@chakra-ui/react";
 import { Envelope } from "../../services/envelopes.service";
 import { Card } from "../Card";
+import { useDeleteEnvelopeMutation } from "../../hooks/envelopes.hooks";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   envelope: Envelope;
 };
 
 export const EnvelopeCard = ({ envelope }: Props): React.ReactElement => {
+  const mutation = useDeleteEnvelopeMutation();
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => {
+    mutation.reset();
+    setIsOpen(false);
+  };
+  const cancelRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (mutation.isError) {
+      setIsOpen(true);
+    }
+  }, [mutation.isError, mutation.error]);
+
   return (
-    <Card>
-      <Text>{envelope.name}</Text>
-    </Card>
+    <>
+      <Card
+        onClickDelete={() => {
+          mutation.mutate(envelope.id);
+        }}
+      >
+        <Text>{envelope.name}</Text>
+      </Card>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Server Error
+            </AlertDialogHeader>
+
+            <AlertDialogBody whiteSpace="pre-line">
+              {`Status Code: ${mutation.error?.response?.data?.statusCode}
+              Message: ${mutation.error?.response?.data?.message}`}
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Ok
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 };
--- src/services/envelopes.service.ts
+++ src/services/envelopes.service.ts
@@ -1,4 +1,4 @@
-import { get, post } from "./base-http.service";
+import { deleteRequest, get, post } from "./base-http.service";
 
 export type Envelope = {
   id: string;
@@ -18,5 +18,10 @@ export async function addEnvelope(
   addEnvelopeDto: AddEnvelopeDto
 ): Promise<Envelope> {
   const response = await post("envelopes", addEnvelopeDto);
+  return response.data;
+}
+
+export async function deleteEnvelope(id: string): Promise<string> {
+  const response = await deleteRequest(`envelopes/${id}`);
   return response.data;
 }
