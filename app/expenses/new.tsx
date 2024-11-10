a50b084356cb733f13a2b904fa9cb61e36819301
Drew Glavich <drewglavich@Drews-MacBook-Pro-2.local>
2024-11-10 20:16:22 +0900

    feat: added react-hook-form and template components to test

--- app/expenses/new.tsx
+++ app/expenses/new.tsx
@@ -1,10 +1,60 @@
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { View } from "react-native";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 import { Text } from "~/components/ui/text";
 
+type Inputs = {
+  firstName: string;
+  lastName: string;
+};
+
 export default function AddExpensePage() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <View>
-      <Text>Add an expense</Text>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            placeholder="First name"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="firstName"
+      />
+      {errors.firstName && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            placeholder="Last name"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="lastName"
+      />
+
+      <Button onPress={handleSubmit(onSubmit)}>
+        <Text>Submit</Text>
+      </Button>
     </View>
   );
 }
